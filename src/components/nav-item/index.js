c8b10d2c8f920b96d8ee779ec22a0760933ab03f
mhmdAljefri <algfry12@gmail.com>
2020-06-27 20:58:08 +0300

    nav items should be visible on mobile view

--- src/components/nav-item/index.js
+++ src/components/nav-item/index.js
@@ -2,10 +2,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "theme-ui"
 import { Link as GatsbyLink } from "gatsby"
-import useIsMobile from "../../hooks/useIsMobile"
 
 function NavItem({ children, to, icon }) {
-  const isMobileView = useIsMobile()
   return (
     <Link
       sx={{
@@ -18,7 +16,7 @@ function NavItem({ children, to, icon }) {
       as={GatsbyLink}
       to={to}
     >
-      {isMobileView || children}
+      {children}
     </Link>
   )
 }
